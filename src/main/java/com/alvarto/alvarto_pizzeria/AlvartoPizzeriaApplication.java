d46809c8b05c0a26ec754b6a686e0addc3d52447
alvarto1296 <alvarto1296@gmail.com>
2024-08-11 13:04:58 -0400

    [create] project Pizzeria

---
+++ src/main/java/com/alvarto/alvarto_pizzeria/AlvartoPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.alvarto.alvarto_pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlvartoPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlvartoPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvarto/alvarto_pizzeria/AlvartoPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvarto.alvarto_pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlvartoPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
